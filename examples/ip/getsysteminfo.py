6628bd9ffe86c5556046e8f3513cd308a02452f3
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-01-10 16:17:14 +0000

    Updated example for IP

--- examples/ip/getsysteminfo.py
+++ examples/ip/getsysteminfo.py
@@ -2,12 +2,9 @@ from betterlib import ip, logging
 
 logger = logging.Logger("./example.log", "ipinfoexample")
 
-logger.info("Your public IP address is %s" % ip.getPubIp())
-logger.info("Your private IP address is %s" % ip.getIp())
-logger.info("Your hostname is %s" % ip.getHostname())
-logger.info("Your MAC address is %s" % ip.getMac())
-logger.info("Your approximate location is %s" % str(ip.getLatLong(ip.getPubIp())))
-logger.info("Your MAC vendor is %s" % ip.getMacVendor(ip.getMac()))
-logger.info("Behind proxy: %s" % ip.isBehindProxy())
-logger.info("Behind tor: %s" % ip.isTor(ip.getPubIp()))
-logger.info("Behind VPN: %s" % ip.isVpn(ip.getPubIp()))
+myip = ip.ipAddress()
+logger.info("Your public IP is: %s" % myip.address)
+logger.info("Your IP is from: %s" % myip.country)
+logger.info("Your IP is from: %s" % myip.city)
+logger.info("Your IP is from: %s" % myip.region)
+logger.info("Your ISP is: %s" % myip.isp)
